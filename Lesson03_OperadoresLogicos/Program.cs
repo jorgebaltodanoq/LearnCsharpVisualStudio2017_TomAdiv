055b54ca86df7123b3bd2d5a40ad7262dbf3db88
Jorge Baltodano <george_itec02@hotmail.com>
2020-08-27 10:38:01 -0500

    cambios

--- Lesson03_OperadoresLogicos/Program.cs
+++ Lesson03_OperadoresLogicos/Program.cs
@@ -1,8 +1,9 @@
 ﻿using System;
+using static System.Console;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
+
 
 namespace Lesson03_OperadoresLogicos
 {
@@ -10,7 +11,22 @@ namespace Lesson03_OperadoresLogicos
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Seguir mañana...");
+            Title = "OPERADORES LOGICOS";
+
+            //1. UTILIZANDO EL OPERADOR AND (&&)
+            //Sirve para hacer doble validacion y que las dos se cumplan si o si
+
+            WriteLine( 7 > 6 && 5 ==5); // Respuesta: TRUE, las dos son verdaderas
+            WriteLine( 2 > 5 && 4 > 3); // Respuesta: FALSE,  una verdad y otro false
+            WriteLine( 5 > 2 && 3 > 4); // Respuesta: FALSE: una verdady otro falso
+            WriteLine( 2 > 5 && 5 != 5); //Respuesta FALSE: Los dos son 
+
+
+
+            //Para mantener activa la consola...
+            WriteLine("\nEnter Close");
+            _ = ReadLine();
+            
         }
     }
 }
